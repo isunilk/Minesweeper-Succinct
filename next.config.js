60b21cc397e9a52390e1ba15a12b43aea8b7b8c3
Sunil Bhuyan <isunil.kb@gmail.com>
2025-03-09 15:26:34 +0530

    package changes

--- next.config.js
+++ next.config.js
@@ -7,22 +7,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    // Allow wasm
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    }
-
-    // Add wasm MIME type
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'webassembly/async'
-    })
-
-    return config
-  }
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
